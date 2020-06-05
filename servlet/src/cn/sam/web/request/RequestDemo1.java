5b813f328cf7f60195128f3ee8ccb24cc1a4978e
Fzsam2019 <www.973837572@qq.com>
2020-06-05 19:47:02 +0800

    2020.6.5

--- servlet/src/cn/sam/web/request/RequestDemo1.java
+++ servlet/src/cn/sam/web/request/RequestDemo1.java
@@ -6,14 +6,71 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.Set;
 
 @WebServlet("/demo1")
 public class RequestDemo1 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getContextPath();
+
+        //预防post出现中文乱码的情况，输入下面代码
+        request.setCharacterEncoding("utf-8");//参数是根据前端页面的编码方式
+
+        String username1 = request.getParameter("username");//获取某个参数值
+        System.out.println(username1);
+        System.out.println("====================");
+        String[] hobbies = request.getParameterValues("hobby");//获取某个参数多个值
+        for (String hobby: hobbies){
+            System.out.println(hobby);
+        }
+        System.out.println("====================");
+        Enumeration<String> parameterNames = request.getParameterNames();//获取一个参数名一个值
+        while (parameterNames.hasMoreElements()){
+            String name = parameterNames.nextElement();
+            System.out.println(name);
+            String value = request.getParameter(name);
+            System.out.println(value);
+            System.out.println("------------------------------------");
+        }
+        System.out.println("====================");
+        Map<String, String[]> parameterMap = request.getParameterMap();//获取一个参数名多个值
+        Set<String> keySet = parameterMap.keySet();
+        for(String name : keySet){
+            String[] values = parameterMap.get(name);
+            System.out.println(name);
+            for (String value : values){
+                System.out.println(value);
+            }
+            System.out.println("-------------------------------------");
+        }
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String contextPath = request.getContextPath();//获取虚拟目录
+        System.out.println(contextPath);
+        System.out.println("------------------");
+        String queryString = request.getQueryString();//
+        System.out.println(queryString);
+        System.out.println("------------------");
+        String requestURI = request.getRequestURI();//获取请求URI，简短：/servlet_war_exploded/demo1
+        StringBuffer requestURL = request.getRequestURL();//获取请求URI，长：http://localhost:8080/servlet_war_exploded/demo1
+        System.out.println(requestURI);
+        System.out.println(requestURL);
+        System.out.println("-------------------");
+//      获取浏览器版本
+        String agent = request.getHeader("user-agent");
+        if (agent.contains("Chrome")){
+            System.out.println("谷歌浏览器");
+        }else if (agent.contains("360")){
+            System.out.println("360浏览器");
+        }
+        System.out.println(agent);
+        //获取从哪里点开连接的地址
+        String referer = request.getHeader("referer");
+        System.out.println(referer);
+        System.out.println("-------------------");
 
     }
 }
--- servlet/src/cn/sam/web/servlet/HttpServlet.java
+++ servlet/src/cn/sam/web/servlet/HttpServlet.java
@@ -6,10 +6,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-//@WebServlet("/httpservlet")
+@WebServlet("/httpservlet")
 //@WebServlet({"/d1","d2","d3"})//多个地址访问
 //@WebServlet("/test/dome")//多层路径访问
-@WebServlet("/*.do")//只要是访问".do"结尾的都可以调用
+//@WebServlet("/*.do")//只要是访问".do"结尾的都可以调用
 public class HttpServlet extends javax.servlet.http.HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
